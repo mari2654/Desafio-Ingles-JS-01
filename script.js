a05a38436456dc785a18a986c3c90e192b0b302c
mari2654 <mariana.santos2654@gmailcom>
2022-06-29 18:35:10 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,33 @@
+let comedy=[
+    {
+        title:"White Chicks",
+        director:"Keenen Ivory Wayans ",
+        year:2004
+    }
+]
+
+   if(title="White Chicks"){
+        console.log("Congrats! You're a funny person");
+   }else{
+        console.log("Ops... You're not that funny person");
+   }
+      
+  
+
+
+let action=[
+    {
+        title:"Mulan",
+        director:"Niki Caro",
+        year:2020
+    }
+]
+
+let fantasy=[
+    {
+        title:"Doctor Strange",
+        director:"Sam Raimi",
+        year:2016
+    }
+]
+
